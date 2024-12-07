9b1a50290614b543b5a7025c73f4b31db903dbd4
Tahaarshad787 <tahaarshad311@gmail.com>
2024-12-07 14:10:36 +0500

    add files and folders

---
+++ app.js
@@ -0,0 +1,162 @@
+// Q1
+// const arrays = [
+//     42,
+//     true,
+//     "Hello world!",
+//     {
+//         name: "John",
+//         age: 25,
+//         isStudent: false,
+//     },
+//     false,
+//     3.14,
+//     "JavaScript is fun!",
+// ];
+
+// const stringArray = arrays.map(element => String(element));
+
+// console.log(stringArray);
+
+// Q2
+// const arrays = [
+//     42,
+//     true,
+//     "Hello world!",
+//     {
+//         name: "John",
+//         age: 25,
+//         isStudent: false,
+//     },
+//     false,
+//     3.14,
+//     "JavaScript is fun!",
+// ];
+
+// const numberArray = arrays.filter(function(element) {
+//     return typeof element === "number";
+// });
+
+// console.log(numberArray);
+
+// Q3
+// const arrays = [
+//     42,
+//     true,
+//     "Hello world!",
+//     {
+//         name: "John",
+//         age: 25,
+//         isStudent: false,
+//     },
+//     false,
+//     3.14,
+//     "JavaScript is fun!",
+// ];
+
+// arrays.forEach(function(element) {
+
+// console.log(typeof element)
+
+// });
+
+// Q4
+// const arrays = [
+//     42,
+//     true,
+//     "Hello world!",
+//     {
+//         name: "John",
+//         age: 25,
+//         isStudent: false,
+//     },
+//     false,
+//     3.14,
+//     "JavaScript is fun!",
+// ];
+
+// const sumOfNumbers = arrays.reduce(function(accumulator, element) {
+
+//     return typeof element === "number" ? accumulator + element : accumulator;
+
+// }, 0);
+
+// console.log(sumOfNumbers);
+
+// Q5
+// const arrays = [
+//     42,
+//     true,
+//     "Hello world!",
+//     {
+//         name: "John",
+//         age: 25,
+//         isStudent: false,
+//     },
+//     false,
+//     3.14,
+//     "JavaScript is fun!",
+// ];
+
+// const firstBoolean = arrays.find(element => typeof element === "boolean");
+
+// console.log(firstBoolean);
+
+// Q6
+// const arrays = [
+//     42,
+//     true,
+//     "Hello world!",
+//     {
+//         name: "John",
+//         age: 25,
+//         isStudent: false,
+//     },
+//     false,
+//     3.14,
+//     "JavaScript is fun!",
+// ];
+
+// const firstObjectIndex = arrays.findIndex(element => typeof element === "object");
+
+// console.log(firstObjectIndex);
+
+// Q7
+// const arrays = [
+//     42,
+//     true,
+//     "Hello world!",
+//     {
+//         name: "John",
+//         age: 25,
+//         isStudent: false,
+//     },
+//     false,
+//     3.14,
+//     "JavaScript is fun!",
+// ];
+
+// const containsNumber = arrays.some(element => typeof element === "number");
+
+// console.log(containsNumber);
+
+// Q8
+// const arrays = [
+//     42,
+//     true,
+//     "Hello world!",
+//     {
+//         name: "John",
+//         age: 25,
+//         isStudent: false,
+//     },
+//     false,
+//     3.14,
+//     "JavaScript is fun!",
+// ];
+
+// const allAreStrings = arrays.every(element => typeof element === "string");
+
+// console.log(allAreStrings);
+
+
+
